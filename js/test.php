3c636634f069c1d777bfd67d21cb08e0ce819480
405nm <405nm.rw@gmail.com>
2018-10-01 02:08:32 +0900

    ()閉じてなかった

--- js/test.php
+++ js/test.php
@@ -33,8 +33,7 @@
 
 //JQuery使って属性を取得する、JSON.parseで扱える形に変換
 var $script = $('#script');
-var result = JSON.parse($script.attr('data-param');
-
+var result = JSON.parse($script.attr('data-param'));
 
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
