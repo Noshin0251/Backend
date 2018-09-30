040f41dd65080a7754827b86f4b60dc22015f0e1
405nm <405nm.rw@gmail.com>
2018-09-30 17:15:20 +0900

    DBから値を得るDBGet.phpを作成

---
+++ php/DBGet.php
@@ -0,0 +1,25 @@
+<?php
+
+//DB接続関数（PDO）
+function db_con(){
+  $dbname='KTYM2018';
+  try {$pdo = new PDO('mysql:dbname=KTYM2018;host=localhost;charset=utf8mb4',' root','lab261admin',
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
+      } catch (PDOException $e) {
+    exit('DbConnectError:'.$e->getMessage());
+  }
+  return $pdo;
+}
+
+
+
+$stmt = $pdo->prepare("SELECT ondo from ondokei where time > current_timestamp + interval -5 SECOND"); //現在時刻から -5秒 のデータのみ取得
+$stmt->execute();
+$res = $stmt->fetchAll();
+
+
+echo count ($res);
+
+
+
+?>
