95f7bdfb13cce7b4c469bb2facfe0890adebc761
405nm <405nm.rw@gmail.com>
2018-10-01 01:42:30 +0900

    GRTDBを修正

--- php/GETDB.php
+++ php/GETDB.php
@@ -1,22 +1,27 @@
-connection_common.php
+
+
 <?php
-require_once 'DbManager.php';
-$pdo = getDb();
-//DB内を表示
-    /*
-    foreach ( $pdo->query ( 'select * from ondokei' ) as $row2 ) {
-    echo '<p>';
-    echo $row2 ['id'], ':';
-    echo $row2 ['ondo'], ':';
-    echo $row2 ['shitudo'];
-    echo '</p>';
-    */
+
+//DB接続関数（PDO）
+function db_con(){
+  $dbname='KTYM2018';
+  try {$pdo = new PDO('mysql:dbname=KTYM2018;host=localhost;charset=utf8mb4','root','lab261admin',
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
+      } catch (PDOException $e) {
+    exit('DbConnectError:'.$e->getMessage());
+  }
+  return $pdo;
+}
+
+
+
 $stmt = $pdo->prepare("SELECT ondo from ondokei where time > (CURRENT_TIMESTAMP + interval -5 SECOND)"); //現在時刻のデータのみ取得
 $stmt->execute();
 $res = $stmt->fetchAll();
 
 
 echo count ($res);
-}
+
+
 
 ?>
