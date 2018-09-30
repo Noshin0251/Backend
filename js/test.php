e5a6a819054203eeb837164ac8028dd6a879ea01
405nm <405nm.rw@gmail.com>
2018-10-01 01:50:22 +0900

    test.phpの読み込むjsのファイルパスを変更

--- js/test.php
+++ js/test.php
@@ -8,9 +8,25 @@
 <canvas id="myChart"></canvas>
 
 
-<script type="text/javascript" src="moment.js"></script>
-<script type="text/javascript" src="Chart.js"></script>
-<script type="text/javascript" src="chartjs-plugin-streaming.js"></script>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- レスポンシブWebデザインを使うために必要なmetaタグ -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap Sample</title>
+    <!-- BootstrapのCSS読み込み -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- BootstrapのJavaScriptよりも、jQueryを先に読み込む
+    そうしなければ、BootstrapのJavaScriptを使う動きが動作しない  -->
+    <!-- jQuery読み込み -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- BootstrapのJS読み込み -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- chart.jsのJS読み込み -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.3.0/Chart.bundle.min.js"></script>
+    
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.js"></script>
+    <script type="text/javascript" src="https://github.com/nagix/chartjs-plugin-streaming/releases/download/v1.2.0/chartjs-plugin-streaming.js"></script>
+    
 <script>
 
 var ctx = document.getElementById('myChart').getContext('2d');
