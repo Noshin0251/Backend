4528299a447624e80c286e0fa5a23ae0576012d6
405nm <405nm.rw@gmail.com>
2018-09-30 17:49:22 +0900

    アクセス完了したときにテキストを追加

--- php/DbManager.php
+++ php/DbManager.php
@@ -4,15 +4,7 @@ function getDb() {
   $dbname='KTYM2018';
   try {$pdo = new PDO('mysql:dbname=KTYM2018;host=localhost;charset=utf8mb4','root','lab261admin',
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
-
-        $sql = 'SELECT * FROM ondokei';
-        $data = $dbh->query($sql);
-    
-        if( !empty($data) ) {
-            foreach( $data as $value ) {
-                var_dump($value['shitudo']);
-            }
-        }
+        print "接続完了:";
 
       } catch (PDOException $e) {
     exit('DbConnectError:'.$e->getMessage());
