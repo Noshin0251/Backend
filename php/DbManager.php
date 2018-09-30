8ed7eed0362b1019b512d0b8df2309d2c8a731ca
405nm <405nm.rw@gmail.com>
2018-09-30 17:32:14 +0900

    謎の空白があったので修正

--- php/DbManager.php
+++ php/DbManager.php
@@ -2,7 +2,7 @@
 
 function getDb() {
   $dbname='KTYM2018';
-  try {$pdo = new PDO('mysql:dbname=KTYM2018;host=localhost;charset=utf8mb4',' root','lab261admin',
+  try {$pdo = new PDO('mysql:dbname=KTYM2018;host=localhost;charset=utf8mb4','root','lab261admin',
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
 
 //        print('接続に成功しました。<br>');
