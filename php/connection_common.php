885ca12ac1716d6710384e15e4443d9d0c5ac07d
405nm <405nm.rw@gmail.com>
2018-09-30 17:54:19 +0900

    温度と湿度をDBから読みだすテスト

--- php/connection_common.php
+++ php/connection_common.php
@@ -2,3 +2,13 @@ connection_common.php
 <?php
 require_once 'DbManager.php';
 $pdo = getDb();
+
+foreach ( $pdo->query ( 'select * from product' ) as $row2 ) {
+    echo '<p>';
+    echo $row2 ['id'], ':';
+    echo $row2 ['ondo'], ':';
+    echo $row2 ['shitudo'];
+    echo '</p>';
+}
+
+?>
