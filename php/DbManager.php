a263a0dc173fcbc486c05a36924e81a00b608b3c
405nm <405nm.rw@gmail.com>
2018-09-30 17:40:59 +0900

    DB接続確認テスト2

--- php/DbManager.php
+++ php/DbManager.php
@@ -5,8 +5,14 @@ function getDb() {
   try {$pdo = new PDO('mysql:dbname=KTYM2018;host=localhost;charset=utf8mb4','root','lab261admin',
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
 
-        print('接続に成功しました。<br>');
-        $pdo->query('SELECT * FROM ondokei');
+        $sql = 'SELECT * FROM ondokei';
+        $data = $dbh->query($sql);
+    
+        if( !empty($data) ) {
+            foreach( $data as $value ) {
+                var_dump($value['shitudo']);
+            }
+        }
 
       } catch (PDOException $e) {
     exit('DbConnectError:'.$e->getMessage());
