c9ed937ea104ca07ae11da2d2976cea2e7d1e4c5
405nm <405nm.rw@gmail.com>
2018-10-01 01:54:56 +0900

    GETDBの値を変更

--- php/GETDB.php
+++ php/GETDB.php
@@ -1,21 +1,21 @@
-
-
 <?php
 
-//DB接続関数（PDO）
-function db_con(){
-  $dbname='KTYM2018';
-  try {$pdo = new PDO('mysql:dbname=KTYM2018;host=localhost;charset=utf8mb4','root','lab261admin',
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
-      } catch (PDOException $e) {
-    exit('DbConnectError:'.$e->getMessage());
+function getDb() {
+    $dbname='KTYM2018';
+    try {$pdo = new PDO('mysql:dbname=KTYM2018;host=localhost;charset=utf8mb4','root','lab261admin',
+          [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
+          print "接続完了:";
+  
+        } catch (PDOException $e) {
+      exit('DbConnectError:'.$e->getMessage());
+      print "エラー!: " . $e->getMessage() . "<br/>";
+    }
+    return $pdo;
   }
-  return $pdo;
-}
 
 
 
-$stmt = $pdo->prepare("SELECT ondo from ondokei where time > (CURRENT_TIMESTAMP + interval -5 SECOND)"); //現在時刻のデータのみ取得
+$stmt = $pdo->prepare("SELECT ondo from ondokei where id = 2"); //現在時刻のデータのみ取得
 $stmt->execute();
 $res = $stmt->fetchAll();
 
@@ -25,3 +25,4 @@ echo count ($res);
 
 
 ?>
+
