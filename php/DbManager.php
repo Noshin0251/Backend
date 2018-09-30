4af20f77793c0f1399b1441a30744550a5c78822
405nm <405nm.rw@gmail.com>
2018-09-30 17:29:50 +0900

    テストに失敗したので修正

--- php/DbManager.php
+++ php/DbManager.php
@@ -5,8 +5,8 @@ function getDb() {
   try {$pdo = new PDO('mysql:dbname=KTYM2018;host=localhost;charset=utf8mb4',' root','lab261admin',
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
 
-        print('接続に成功しました。<br>');
-        $pdo->query('SELECT * FROM ondokei');
+//        print('接続に成功しました。<br>');
+//        $pdo->query('SELECT * FROM ondokei');
 
       } catch (PDOException $e) {
     exit('DbConnectError:'.$e->getMessage());
