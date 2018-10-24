6c07b66130ddcbfbb2028fee961893e28fb72d6b
405nm <405nm.rw@gmail.com>
2018-10-25 02:12:29 +0900

    connection_common.phpの関数に引数を用意する

--- js/connection_common.php
+++ js/connection_common.php
@@ -12,13 +12,18 @@ foreach ( $pdo->query ( 'select * from ondokei' ) as $row2 ) {
 }
 
 
-function insert_data(){
+function insert_data($r_ondo,$r_shitudo,$r_level,$r_comment){
+
+if (is_null($r_ondo)){echo 'ラズパイからの温度がNULLです。';}
+if (is_null($r_shitudo)){echo 'ラズパイからの湿度がNULLです。';}
+if (is_null($r_level)){echo 'ラズパイからの暑さ指数がNULLです。';}
+if (is_null($r_comment)){echo 'ラズパイからのコメントがNULLです。';}
 // INSERT文を変数に格納
 $sql = "INSERT INTO ondokei (ondo,shitudo,level,comment) VALUES (:ondo, :shitudo, :level, :comment)";
 // 挿入する値は空のまま、SQL実行の準備をする
 $stmt = $pdo -> prepare($sql);
 // 挿入する値を配列に格納する
-$params = array(':ondo' => '15', ':shitudo' => '15', ':level' => '5', ':comment' => 'phpから挿入テスト');
+$params = array(':ondo' => $r_ondo, ':shitudo' => $r_shitudo, ':level' => $r_level, ':comment' => $r_comment);
 // 挿入する値が入った変数をexecuteにセットしてSQLを実行
 $stmt->execute($params);
 // 登録完了のメッセージ
